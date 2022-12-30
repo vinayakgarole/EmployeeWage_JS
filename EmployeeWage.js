affade50b1afcfa6b093273ae19df25905cd7a5a
vinayakgarole <vinayakgarole10@gamil.com>
2022-12-30 16:51:50 +0530

    Added: Check Employee Present or Absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,11 @@
 function getAttendance() {
 
     let checkStatus =  Math.floor(Math.random() * 10) %  2;
-    
-    if(checkStatus == 0)
-        console.log("Employee is absent");
-    else
-        console.log("Employee is present");
-    }
 
+    if(checkStatus == 0) {
+        console.log("Employee is Absent");
+    } else {
+        console.log("Employee is Present");
+    }
+}
     getAttendance();
