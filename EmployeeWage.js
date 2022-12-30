49b68c1e44d9a06c7ffd5c6583c01370d9c0216d
vinayakgarole <vinayakgarole10@gamil.com>
2022-12-30 16:56:18 +0530

    Added: Check employee wage for particular time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -8,4 +8,36 @@ function getAttendance() {
         console.log("Employee is Present");
     }
 }
+
     getAttendance();
+
+    const   PART_TIME_PRESENT = 2;
+    const   PART_TIME_WORK_HR = 4;
+    const   FULL_TIME_PRESENT = 1;
+    const   FULL_TIME_WORK_HR = 8;
+    const   SALARY_PER_HR = 20;
+
+    function getPartTime() {
+        let workHr = 0;
+        let dailyWage = 0;
+        let emp = Math.floor(Math.random() * 10) %  3;
+
+        if (emp == 	FULL_TIME_PRESENT) {
+        console.log("Employee is present");
+        workHr = FULL_TIME_WORK_HR;
+        }
+        else if (emp == PART_TIME_PRESENT) {
+            console.log("Employee is part time");
+            workHr = PART_TIME_WORK_HR;
+        }
+        else{
+        console.log("Employee is absent");
+        workHr = 0;
+        }
+        
+        dailyWage= (SALARY_PER_HR * workHr);
+        console.log("salary will be :" + dailyWage);
+        
+    }
+
+    getPartTime();
