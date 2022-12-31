f275545dca0b9bcd4af8c79efe1d1e5cd1721eeb
vinayakgarole <vinayakgarole10@gamil.com>
2022-12-31 10:11:38 +0530

    Added: Calculate Wages For month with 20 Working days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -23,7 +23,7 @@ function getAttendance() {
         let emp = Math.floor(Math.random() * 10) %  3;
 
         if (emp == 	FULL_TIME_PRESENT) {
-        console.log("Employee is present");
+        console.log("Employee is Present");
         workHr = FULL_TIME_WORK_HR;
         }
         else if (emp == PART_TIME_PRESENT) {
@@ -31,7 +31,7 @@ function getAttendance() {
             workHr = PART_TIME_WORK_HR;
         }
         else{
-        console.log("Employee is absent");
+        console.log("Employee is Absent");
         workHr = 0;
         }
         
@@ -41,3 +41,42 @@ function getAttendance() {
     }
 
     getPartTime();
+
+    function getWagePerMonth() {
+			
+        let empHr = 0;
+        let total = 0;
+        let days = 0;
+        let workHr = 0;
+        
+        while( days<20 ) {
+        let emp =  Math.floor(Math.random() * 10) %  3;
+        
+        switch(emp) { 
+            case 0:
+                console.log("Employee is Present");
+                empHr = 8;
+                break;
+            case 1:
+                console.log("Employee is PartTime ");
+                empHr = 4;
+                break;
+            case 2:
+                console.log("Employee is Absent");
+                empHr = 0;
+                break;
+            default:
+                console.log("contition does not exit");
+        }
+        let dailyWage = (SALARY_PER_HR * empHr);
+        total = (total + dailyWage);
+        days = (days + 1);
+        workHr = (workHr + empHr);
+        }
+        console.log("total salary will be :" + total);
+    
+        console.log("total working hours will be:" + workHr);
+        
+    }
+
+    getWagePerMonth();
